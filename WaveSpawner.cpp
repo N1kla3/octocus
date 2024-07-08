27894a6c3d0c72f32fbc08a47c3be7b6e42bb1db
mikalai <kolya.vladimirsky@gmail.com>
2024-07-08 21:14:08 +0300

    Update cmake for linux, add entity spawn in wave spawner

--- WaveSpawner.cpp
+++ WaveSpawner.cpp
@@ -1,18 +1,40 @@
 #include "WaveSpawner.h"
+#include "LifeComponents.h"
+#include "SpaceComponents.h"
+#include "RenderComponent.h"
+#include "raylib.h"
+#include "WeaponComponent.h"
 
-void WaveSpawner::spawnRangeEnemy()
+void WaveSpawner::spawnRangeEnemy(entt::registry& registry)
 {
+    auto entity = registry.create();
+    registry.emplace<Health>(entity, 10, 10);
+    registry.emplace<position>(entity, 10.f, 10.f);
+    registry.emplace<velocity>(entity, 0.f, 0.f);
+    registry.emplace<sphere_collision>(entity, 10.f);
+    registry.emplace<RenderComponent>(entity, BLUE, 10.f, RenderPriority::MIDLE);
+    registry.emplace<WeaponComponent>(entity, 10.f, 10.f, 2.f, true);
 
 }
 
-void WaveSpawner::spawnMeleeEnemy()
+void WaveSpawner::spawnMeleeEnemy(entt::registry& registry)
 {
-
+    auto entity = registry.create();
+    registry.emplace<Health>(entity, 10, 10);
+    registry.emplace<position>(entity, 10.f, 10.f);
+    registry.emplace<velocity>(entity, 0.f, 0.f);
+    registry.emplace<sphere_collision>(entity, 10.f);
+    registry.emplace<RenderComponent>(entity, BLUE, 10.f, RenderPriority::MIDLE);
 }
 
-void WaveSpawner::startWave()
+void WaveSpawner::startWave(entt::registry& registry)
 {
-
+    if (m_WaveCounter < s_Waves.size())
+    {
+        m_EnemiesToSpawn = s_Waves[m_WaveCounter];
+        spawnRangeEnemy(registry);
+        spawnMeleeEnemy(registry);
+    }
 }
 
 void WaveSpawner::update(float delta)
@@ -20,7 +42,3 @@ void WaveSpawner::update(float delta)
 
 }
 
-bool WaveSpawner::checkWaveFinished()
-{
-    return false;
-}
--- WaveSpawner.h
+++ WaveSpawner.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <array>
+#include "entt/entt.hpp"
 
 class WaveSpawner
 {
     inline static std::array<int, 12> s_Waves = { 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240 };
 private:
-    void spawnRangeEnemy();
-    void spawnMeleeEnemy();
+    void spawnRangeEnemy(entt::registry& registry);
+    void spawnMeleeEnemy(entt::registry& registry);
 
-    int m_WaveCounter = 0;
+    size_t m_WaveCounter = 0;
+    int m_EnemiesToSpawn = 0;
 
 public:
-    void startWave();
-    bool checkWaveFinished();
+    void startWave(entt::registry& registry);
     void update(float delta);
 };
+
